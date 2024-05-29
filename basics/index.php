229ba90d1f4e6d4239abe956201ab4c32676e0c4
tuhinWebValy <tuhin@webvaly.com>
2024-05-29 15:51:51 +0600

    Sum Using fUNCTION

--- basics/index.php
+++ basics/index.php
@@ -3,19 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Learning PHP</title>
+    <title>Basics PHP</title>
 </head>
 <body>
-    <?php
-        $text =  'Learning PHP'
-    ?>
+    <!-- Varable -->
+        <!-- Basic Sum -->
+        <?php
+            $num1 = 5;
+            $num2 = 15;
 
-    <h2>I'm Start <?php echo $text ?></h2>
+            $sum =  $num1 + $num2;
+            echo "<p> Basic Sum of num1 & num2 = $sum</p>";
 
-    <?php
-        echo "<p>I'm start Learning $text </p>";
-    ?>
+            function sumFn($num1,$num2){
+                return $num1 + $num2;
+            }
 
+            echo "<p> Basic Sum using Function = ". sumFn($num1,$num2)."</p>";
+        ?>
 
+        <!-- Sum With function -->
+
+        
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP</title>
+</head>
+<body>
+    
+    <?php
+        $text =  'Learning PHP'
+    ?>
+
+    <h2>I'm Start <?php echo $text ?></h2>
+
+    <?php
+        echo "<p>I'm start Learning $text </p>";
+    ?>
+
+    <p><a href="http://localhost/php-learning/basics/">Basics</a></p>
+
+
+</body>
+</html>
